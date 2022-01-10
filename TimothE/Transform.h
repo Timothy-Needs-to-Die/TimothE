ae603fde9781d370213617573d3d673bb137eb41
domdavies <56914703+domdavies@users.noreply.github.com>
2022-01-10 22:43:45 +0000

    simple Tranform class
    Added in a quick and easy Transform class in order to get started

--- TimothE/Transform.h
+++ TimothE/Transform.h
@@ -1,5 +1,34 @@
 #pragma once
-class Transform
+
+struct Vector2D
 {
+	float _x, _y;
+
+	Vector2D(float x, float y)
+	{
+		_x = x;
+		_y = y;
+	}
 };
 
+class Transform
+{
+public:
+	Transform();
+	Transform(float x, float y, float rotx, float roty, float scalex, float scaley);
+
+	Vector2D* GetPosition() { return new Vector2D(_mXpos, _mYpos); };
+	void SetPosition(float x, float y);
+	float GetXrotation() { return _mXrot; };
+	void SetXrotation(float xRot);
+
+	~Transform();
+
+private:
+	float _mXpos;
+	float _mYpos;
+	float _mXrot;
+	float _mYrot;
+	float _mScaleX;
+	float _mScaleY;
+};
