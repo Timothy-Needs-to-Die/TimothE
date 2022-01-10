0960e165ee6a029332362c4d44d0e2dc026f1108
Jack Dobie <57402187+JackDobie@users.noreply.github.com>
2022-01-10 16:39:47 +0000

    Create gameobject class

--- TimothE/GameObject.cpp
+++ TimothE/GameObject.cpp
@@ -1,1 +1,28 @@
 #include "GameObject.h"
+
+GameObject::GameObject()
+{
+	//_pTransform = new Transform();
+}
+
+GameObject::GameObject(Transform* transform)
+{
+	_pTransform = transform;
+}
+
+GameObject::~GameObject()
+{
+	delete(_pTransform);
+}
+
+Component* GameObject::GetComponent(string name)
+{
+	/*for (Component* c : _components)
+	{
+		if (c->GetName() == name)
+		{
+			return c;
+		}
+	}*/
+	return nullptr;
+}
--- TimothE/GameObject.h
+++ TimothE/GameObject.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <vector>
+#include <string>
+#include "Component.h"
+#include "Transform.h"
+
+using std::vector;
+using std::string;
+
 class GameObject
 {
-};
+public:
+	GameObject();
+	GameObject(Transform* transform);
+	~GameObject();
 
+	Component* GetComponent(string name);
+	vector<Component*> GetComponents() { return _components; }
+	Transform* GetTransform() { return _pTransform; }
+private:
+	vector<Component*> _components;
+	Transform* _pTransform;
+};
