b4f2443cd1acd82f6249c52ae5f8d1af6911266d
Jack Dobie <57402187+JackDobie@users.noreply.github.com>
2022-01-11 14:43:03 +0000

    Add object tag, uid & start update exit functions

--- TimothE/GameObject.cpp
+++ TimothE/GameObject.cpp
@@ -1,18 +1,53 @@
 #include "GameObject.h"
 
-GameObject::GameObject()
+GameObject::GameObject(string name, ObjectType tag)
 {
-	//_pTransform = new Transform();
+	_name = name;
+	_tag = tag;
+	_UID = UID::GenerateUID();
+	_pTransform = new Transform();
+	Start();
 }
 
-GameObject::GameObject(Transform* transform)
+GameObject::GameObject(string name, ObjectType tag, Transform* transform)
 {
+	_name = name;
+	_tag = tag;
 	_pTransform = transform;
 }
 
 GameObject::~GameObject()
 {
+	Exit();
+	for (Component* c : _components)
+	{
+		delete(c);
+	}
 	delete(_pTransform);
+}
+
+void GameObject::Start()
+{
+	/*for (Component* c : _components)
+	{
+		c->Start();
+	}*/
+}
+
+void GameObject::Update(float deltaTime)
+{
+	/*for (Component* c : _components)
+	{
+		c->Update();
+	}*/
+}
+
+void GameObject::Exit()
+{
+	/*for (Component* c : _components)
+	{
+		c->Exit();
+	}*/
 }
 
 Component* GameObject::GetComponent(string name)
@@ -25,4 +60,20 @@ Component* GameObject::GetComponent(string name)
 		}
 	}*/
 	return nullptr;
+}
+Component* GameObject::GetComponent(string ID)
+{
+	/*for (Component* c : _components)
+	{
+		if (c->GetUID() == ID)
+		{
+			return c;
+		}
+	}*/
+	return nullptr;
+}
+
+void GameObject::AddComponent(Component* component)
+{
+	_components.push_back(component);
 }
--- TimothE/GameObject.h
+++ TimothE/GameObject.h
@@ -3,21 +3,42 @@
 #include <string>
 #include "Component.h"
 #include "Transform.h"
+#include "UID.h"
 
 using std::vector;
 using std::string;
 
+enum class ObjectType
+{
+	Player,
+	Enemy,
+	NPC,
+	PickUp
+};
+
 class GameObject
 {
 public:
-	GameObject();
-	GameObject(Transform* transform);
+	GameObject(string name, ObjectType tag);
+	GameObject(string name, ObjectType tag, Transform* transform);
 	~GameObject();
 
+	void Start();
+	void Update(float deltaTime);
+	void Exit();
+
+	string GetUID() { return _UID; }
+	string GetName() { return _name; }
 	Component* GetComponent(string name);
+	Component* GetComponent(string ID);
 	vector<Component*> GetComponents() { return _components; }
 	Transform* GetTransform() { return _pTransform; }
+
+	void AddComponent(Component* component);
 private:
+	string _UID;
+	string _name;
+	ObjectType _tag;
 	vector<Component*> _components;
 	Transform* _pTransform;
 };
