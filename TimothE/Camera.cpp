67c256279250637990dca471aeddc02865e30061
Isaac Dunn <57407700+IsaacdmDunn@users.noreply.github.com>
2022-01-13 14:15:14 +0000

    Camera class for movement

--- TimothE/Camera.cpp
+++ TimothE/Camera.cpp
@@ -1,5 +1,4 @@
 #include "Camera.h"
-#include <GL/glew.h>
 
 void Camera::Onstart()
 {
@@ -10,8 +9,12 @@ void Camera::OnUpdate()
 	//updates camera position and rotation
 	glLoadIdentity();
 	gluLookAt(_mTransform.GetPosition()->_x, _mTransform.GetPosition()->_y,0.0f,
-			  _mTransform.GetPosition()->_x + _mTransform.GetXrotation(), _mTransform.GetPosition()->_y + _mTransform.GetXrotation(),0,
+			  _mTransform.GetPosition()->_x , _mTransform.GetPosition()->_y + _mTransform.GetXrotation(),0,
 				1.0f,0,0);
+	glRotatef(_mTransform.GetXrotation(), 1, 0, 0);
+	glRotatef(_mTransform.GetXrotation(), 0, 1, 0);//y
+	glRotatef(_mTransform.GetXrotation(), 0, 0, 1);//z
+	glTranslatef(_mTransform.GetPosition()->_x, _mTransform.GetPosition()->_y, 0);
 }
 
 void Camera::OnEnd()
--- TimothE/Camera.h
+++ TimothE/Camera.h
@@ -1,9 +1,16 @@
 #include "Transform.h"
+
+#include <GL/glew.h>
+#include <glm.hpp>
 class Camera {
+public:
 	Camera(Transform transform) { _mTransform = transform; }
 	~Camera() {}
 	void Onstart();
 	void OnUpdate();
 	void OnEnd();
 	Transform _mTransform;
+	glm::vec3 _mEye = {0,0,0};
+	glm::vec3 _mCenter = { 0,0,0 };
+	glm::vec3 _mUp = { 0,0,0 };
 };
