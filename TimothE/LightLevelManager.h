e6edea33531ced459a3fd8a2e8b6d61ff7173184
Jacob <jacobdexter43@outlook.com>
2022-05-19 13:42:42 +0100

    Vector issue.

--- TimothE/LightLevelManager.h
+++ TimothE/LightLevelManager.h
@@ -14,11 +14,10 @@ struct LightSource
 
 class LightLevelManager
 {
+private:
+	std::vector<LightSource> _lightSources;
 public:
 	void AddLightSource(LightSource lightSource);
 	void RemoveLightSource(LightSource lightSource);
 	void UpdateLightMap(TileMap* tileMap);
-
-private:
-	std::vector<LightSource> _lightSources;
 };
