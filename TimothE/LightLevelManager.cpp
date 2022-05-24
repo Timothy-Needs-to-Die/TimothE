96a6613a70caa8ddc0e178b53a2ed059f2839622
Jacob <jacobdexter43@outlook.com>
2022-05-24 14:32:14 +0100

    :)

--- TimothE/LightLevelManager.cpp
+++ TimothE/LightLevelManager.cpp
@@ -11,30 +11,39 @@ void LightLevelManager::UpdateLightMap(TileMap* tileMap)
 
 		for (auto const& source : _lightSources)
 		{
-			//tileMap->GetTileIndexFromPosition
-
-			//needs to loop through layers
-			//xMin = tileMap->GetTileAtWorldPos(0, glm::vec2(source.worldPos.x - (source.range * tileSize), source.worldPos.y))->pos.x;
-			//xMax = tileMap->GetTileAtWorldPos(0, glm::vec2(source.worldPos.x + (source.range * tileSize), source.worldPos.y))->pos.x;
-			//yMin = tileMap->GetTileAtWorldPos(0, glm::vec2(source.worldPos.x, source.worldPos.y - (source.range * tileSize)))->pos.y;
-			//yMax = tileMap->GetTileAtWorldPos(0, glm::vec2(source.worldPos.x, source.worldPos.y + (source.range * tileSize)))->pos.y;
-
-			xMin = tileMap->GetTileAtWorldPos(0, glm::vec2(2.0f, 2.0f))->pos.x - 30.0f;
-			xMax = tileMap->GetTileAtWorldPos(0, glm::vec2(2.0f, 2.0f))->pos.x + 30.0f;
-			yMin = tileMap->GetTileAtWorldPos(0, glm::vec2(2.0f, 2.0f))->pos.y - 30.0f;
-			yMax = tileMap->GetTileAtWorldPos(0, glm::vec2(2.0f, 2.0f))->pos.y + 30.0f;
-
-			for (float y = yMin; y <= yMax; y += tileSize)
+			for (int i = 0; i < 6; i++)
 			{
-				for (float x = xMin; x <= xMax; x += tileSize)
+				//tileMap->GetTileIndexFromPosition
+
+				//needs to loop through layers
+				xMin = tileMap->GetTileAtWorldPos(i, glm::vec2(source.worldPos.x - (source.range * tileSize), source.worldPos.y))->pos.x;
+				xMax = tileMap->GetTileAtWorldPos(i, glm::vec2(source.worldPos.x + (source.range * tileSize), source.worldPos.y))->pos.x;
+				yMin = tileMap->GetTileAtWorldPos(i, glm::vec2(source.worldPos.x, source.worldPos.y - (source.range * tileSize)))->pos.y;
+				yMax = tileMap->GetTileAtWorldPos(i, glm::vec2(source.worldPos.x, source.worldPos.y + (source.range * tileSize)))->pos.y;
+
+				//xMin = tileMap->GetTileAtWorldPos(i, glm::vec2(2.0f, 2.0f))->pos.x - 30.0f;
+				//xMax = tileMap->GetTileAtWorldPos(i, glm::vec2(2.0f, 2.0f))->pos.x + 30.0f;
+				//yMin = tileMap->GetTileAtWorldPos(i, glm::vec2(2.0f, 2.0f))->pos.y - 30.0f;
+				//yMax = tileMap->GetTileAtWorldPos(i, glm::vec2(2.0f, 2.0f))->pos.y + 30.0f;
+
+				for (float y = yMin; y <= yMax; y += tileSize)
 				{
-					TileData* tile = tileMap->GetTileAtWorldPos(0, glm::vec2(x, y));
+					for (float x = xMin; x <= xMax; x += tileSize)
+					{
+						TileData* tile = tileMap->GetTileAtWorldPos(i, glm::vec2(x, y));
 
-					//get affective light value = baselight - (falloff * distance of tiles between source and tile rounded)
-					int affectiveValue = source.baseLightLevel - (source.fallOffRate * (int)round(glm::distance(glm::vec2(x, y), source.worldPos) / tileSize));
+						//get affective light value = baselight - (falloff * distance of tiles between source and tile rounded)
+						int affectiveValue = source.baseLightLevel - (source.fallOffRate * (int)round(glm::distance(glm::vec2(x, y), source.worldPos) / tileSize));
 
-					//get final light value and apply to tile
-					tile->lightLevel += affectiveValue;
+						if (tile->lightLevel + affectiveValue < 2)
+						{
+							tile->lightLevel = 2;
+							continue;
+						}
+
+						//get final light value and apply to tile
+						tile->lightLevel += affectiveValue;
+					}
 				}
 			}
 		}
