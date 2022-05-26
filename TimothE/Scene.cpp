71985e075288458728a15084cc72137524e79072
Adam Harris <32218978+AdamHarris-GamesProgrammer@users.noreply.github.com>
2022-05-26 11:30:33 +0100

    Fix

--- TimothE/Scene.cpp
+++ TimothE/Scene.cpp
@@ -138,11 +138,12 @@ void Scene::RenderScene(Camera* cam)
 	Renderer2D::BeginRender(cam);
 
 
-	for (std::vector<GameObject*>::iterator it = _listOfDrawableGameObjects.end() - 1; it != _listOfDrawableGameObjects.begin(); --it) {
+	for (std::vector<GameObject*>::iterator it = _listOfDrawableGameObjects.begin(); it != _listOfDrawableGameObjects.end(); ++it) {
 		GameObject* obj = *it;
 
 		if (!obj->IsActive()) continue;
 		if (obj->IsToBeDestroyed()) continue;
+		if (obj->GetTag() == "PLAYER") continue;
 
 		//TODO: Text won't render here as it uses its own internal texture data.
 		Texture2D* objTex = obj->GetComponent<Texture2D>();
@@ -162,6 +163,12 @@ void Scene::RenderScene(Camera* cam)
 				Renderer2D::DrawQuad(obj->GetTransform()->GetRenderQuad(), objTex);
 			}
 		}
+	}
+
+	GameObject* player = FindObjectWithTag("PLAYER");
+	if (player) {
+		SpriteComponent* sc = player->GetComponent<SpriteComponent>();
+		Renderer2D::DrawQuad(player->GetTransform()->GetRenderQuad(), sc->GetSprite()->GetTexture(), sc->GetSprite()->GetTexCoords());
 	}
 
 	Renderer2D::EndRender();
