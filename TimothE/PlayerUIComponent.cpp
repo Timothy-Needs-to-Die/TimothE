1325ea2aa3923d0f50262a0dd08e39312f150eff
Ryan Goldsmith <50884845+ryangoldsmith@users.noreply.github.com>
2022-05-27 11:56:12 +0100

    Updated farm controls and moved to bottom of screen

--- TimothE/PlayerUIComponent.cpp
+++ TimothE/PlayerUIComponent.cpp
@@ -25,19 +25,25 @@ PlayerUIComponent::PlayerUIComponent(GameObject* parent)
 	SetBuildModeUIActive(false);
 
 	TextObject* pFarmingModeTitle = new TextObject("Farming Mode", "arial.ttf");
-	pFarmingModeTitle->GetTransform()->SetPosition(800.0f, 1000.0f);
+	pFarmingModeTitle->GetTransform()->SetPosition(800.0f, 200.0f);
 	pScene->AddGameObject(pFarmingModeTitle);
 	_pFarmModeUI.emplace_back(pFarmingModeTitle);
 
 	std::string farmText = "F: Till / Harvest   1: Wheat   2: Potato";
 	TextObject* pFarmControls = new TextObject(farmText, "arial.ttf");
-	pFarmControls->GetTransform()->SetPosition(600.0f, 950.0f);
+	pFarmControls->GetTransform()->SetPosition(600.0f, 150.0f);
 	pScene->AddGameObject(pFarmControls);
 	_pFarmModeUI.emplace_back(pFarmControls);
 	
-	farmText = "3: Carrot   4: Demolish Plot\nF1: Sell all Crops";
+	farmText = "3: Carrot   4: Demolish Plot";
 	pFarmControls = new TextObject(farmText, "arial.ttf");
-	pFarmControls->GetTransform()->SetPosition(700.0f, 900.0f);
+	pFarmControls->GetTransform()->SetPosition(700.0f, 100.0f);
+	pScene->AddGameObject(pFarmControls);
+	_pFarmModeUI.emplace_back(pFarmControls);
+
+	farmText = "Buy Controls : F1: wheat seed  F2 : potato seed  F3 : Carrot Seed  F4 : sell all produce";
+	pFarmControls = new TextObject(farmText, "arial.ttf");
+	pFarmControls->GetTransform()->SetPosition(50.0f, 50.0f);
 	pScene->AddGameObject(pFarmControls);
 	_pFarmModeUI.emplace_back(pFarmControls);
 
