b1b01f556e4323348c9e494c31b3cffe0e3aea6d
Adam Harris <adam_harris@hotmail.com>
2022-07-21 16:34:01 +0100

    Auto translate system

--- TimothETranslationSystem/TimothETranslationSystem/Source.cpp
+++ TimothETranslationSystem/TimothETranslationSystem/Source.cpp
@@ -7,31 +7,24 @@
 #include <algorithm>
 #include <map>
 #include "CSVReader.h"
+#include "Dirent.h"
 
-template<typename A, typename B>
-std::pair<B, A> flip_pair(const std::pair<A, B>& p)
-{
-	return std::pair<B, A>(p.second, p.first);
-}
 
-template<typename A, typename B>
-std::multimap<B, A> flip_map(const std::map<A, B>& src)
-{
-	std::multimap<B, A> dst;
-	std::transform(src.begin(), src.end(), std::inserter(dst, dst.begin()),
-		flip_pair<A, B>);
-	return dst;
-}
+//TODO: Write more dialogs files
+//TODO: Read in all dialog files 
+//TODO: Output all tokened files
+//TODO: Documentation
+
 
 int main() {
 	std::map<std::string, int> dictionary;
-	
+
 
 	std::map<int, std::string> inputDict;
-
+	std::map<int, std::string> outputDict;
 	std::vector<std::vector<std::string>> dictionaryContents = CSVReader::RequestDataFromFile("Dictionary.csv");
 
-	
+
 	int lastRow;
 	int index = 0;
 	for (auto& line : dictionaryContents)
@@ -53,140 +46,152 @@ int main() {
 
 	lastRow = index;
 
-
-	std::ifstream file("SampleDialogue.txt");
-
-	std::vector<std::string> lines;
-	std::vector<std::string> tokenisedLines;
-
-	std::stringstream fileStream;
-
-	fileStream << file.rdbuf();
-
-	std::string line;
-	while (std::getline(fileStream, line, '\n'))
+	DIR* directory = opendir("./Dialog/");
+	struct dirent* dirent;
+	if (directory)
 	{
-		lines.emplace_back(line);
-	}
+		while ((dirent = readdir(directory)) != NULL)
+		{
+			if (dirent->d_type == 32768)
+			{
+				std::string fn = "Dialog/" + std::string(dirent->d_name);
+				std::ifstream file(fn);
 
-	for (auto& l : lines) {
-		std::string tokenisedLine;
+				std::vector<std::string> lines;
+				std::vector<std::string> tokenisedLines;
 
 
+				std::stringstream fileStream;
 
-		int leftBracePos = l.find_first_of("[");
-		int rightBracePos = l.find_first_of("]");
+				fileStream << file.rdbuf();
 
-		std::string characterName = l.substr(leftBracePos, rightBracePos + 1);
-		tokenisedLine += characterName + ": ";
-
-		int lastSpeechMark = l.find_last_of('"');
-		std::string eolContent = l.substr(lastSpeechMark + 1);
-
-		int markAPos;
-		std::string filterString = l.substr(rightBracePos + 2);
-		while (markAPos = filterString.find('"') != std::string::npos) {
-
-			filterString = filterString.substr(markAPos - 1);
-
-			int markBPos = filterString.find('"');
-
-			std::string sentence = filterString.substr(0, markBPos);
-
-			if (sentence == "") {
-				continue;
-			}
-
-			//Converts hello world to HELLO WORLD
-			std::string upperedSentence;
-			for (int i = 0; i < sentence.length(); i++) {
-				upperedSentence += std::toupper(sentence[i]);
-			}
-
-			//Converts HELLO WORLD to a vector of (HELLO), (WORLD). Also parses punctuation
-			std::vector<std::string> brokenSentence;
-			std::istringstream ss(upperedSentence);
-			std::string token;
-
-			while (std::getline(ss, token, ' ')) {
-				int pos = token.find('?');
-				if (pos != std::string::npos)
+				std::string line;
+				while (std::getline(fileStream, line, '\n'))
 				{
-					token = token.substr(0, pos);
-					brokenSentence.emplace_back("?");
-				}
-				pos = token.find('.');
-				if (pos != std::string::npos) {
-					token = token.substr(0, pos);
-					brokenSentence.emplace_back(".");
+					lines.emplace_back(line);
 				}
 
-				//Places the token into the broken sentence list
-				brokenSentence.emplace_back(token);
+				for (auto& l : lines) {
+					std::string tokenisedLine;
+
+					int leftBracePos = l.find_first_of("[");
+					int rightBracePos = l.find_first_of("]");
+
+					std::string characterName = l.substr(leftBracePos, rightBracePos + 1);
+					tokenisedLine += characterName + ": ";
+
+					int lastSpeechMark = l.find_last_of('"');
+					std::string eolContent = l.substr(lastSpeechMark + 1);
+
+					int markAPos;
+					std::string filterString = l.substr(rightBracePos + 2);
+					while (markAPos = filterString.find('"') != std::string::npos) {
+
+						filterString = filterString.substr(markAPos - 1);
+
+						int markBPos = filterString.find('"');
+
+						std::string sentence = filterString.substr(0, markBPos);
+
+						if (sentence == "") {
+							continue;
+						}
+
+						//Converts hello world to HELLO WORLD
+						std::string upperedSentence;
+						for (int i = 0; i < sentence.length(); i++) {
+							upperedSentence += std::toupper(sentence[i]);
+						}
+
+						//Converts HELLO WORLD to a vector of (HELLO), (WORLD). Also parses punctuation
+						std::vector<std::string> brokenSentence;
+						std::istringstream ss(upperedSentence);
+						std::string token;
+
+						while (std::getline(ss, token, ' ')) {
+							int pos = token.find('?');
+							if (pos != std::string::npos)
+							{
+								token = token.substr(0, pos);
+								brokenSentence.emplace_back("?");
+							}
+							pos = token.find('.');
+							if (pos != std::string::npos) {
+								token = token.substr(0, pos);
+								brokenSentence.emplace_back(".");
+							}
+
+							//Places the token into the broken sentence list
+							brokenSentence.emplace_back(token);
+						}
+
+						//Cycles through all tokens
+						for (auto& word : brokenSentence) {
+							if (word.empty()) continue;
+
+							//If the word is game data then it is added to the tokenised line and the next word is checked
+							if (word[0] == '#') {
+								tokenisedLine += word + " ";
+								continue;
+							}
+							else if (word[0] == '~') {
+								tokenisedLine += word + " ";
+								continue;
+							}
+							else if (word[0] == '[') {
+								tokenisedLine += word + " ";
+								continue;
+							}
+							else if (word[0] == '-') {
+								tokenisedLine += word + " ";
+								continue;
+							}
+
+							//If the word cannot be found in our dictionary
+							if (dictionary.find(word) == dictionary.end()) {
+								dictionary[word] = index; //Add this word to the dictionary with our new index
+								tokenisedLine += std::to_string(index) + " "; //Adds this index to the tokenised line
+								index++; //Increments our index
+							}
+							else {
+								tokenisedLine += std::to_string(dictionary[word]) + " "; //Adds this words token to the line
+							}
+						}
+
+
+						filterString = filterString.substr(markBPos + 1);
+						markAPos = filterString.find('"');
+
+					}
+
+					tokenisedLine += eolContent;
+
+					tokenisedLines.emplace_back(tokenisedLine);
+				}
+
+
+				for (auto& el : dictionary) {
+					outputDict[el.second] = el.first;
+				}
+
+				
+				std::ofstream output("TranslatedDialog/" + std::string(dirent->d_name));
+
+				for (auto& tl : tokenisedLines) {
+					std::cout << tl << std::endl;
+					output << tl << "\n";
+				}
 			}
-
-			//Cycles through all tokens
-			for (auto& word : brokenSentence) {
-				if(word.empty()) continue;
-
-				//If the word is game data then it is added to the tokenised line and the next word is checked
-				if (word[0] == '#') {
-					tokenisedLine += word + " ";
-					continue;
-				}
-				else if (word[0] == '~') {
-					tokenisedLine += word + " ";
-					continue;
-				}
-				else if (word[0] == '[') {
-					tokenisedLine += word + " ";
-					continue;
-				}
-				else if (word[0] == '-') {
-					tokenisedLine += word + " ";
-					continue;
-				}
-
-				//If the word cannot be found in our dictionary
-				if (dictionary.find(word) == dictionary.end()) {
-					dictionary[word] = index; //Add this word to the dictionary with our new index
-					tokenisedLine += std::to_string(index) + " "; //Adds this index to the tokenised line
-					index++; //Increments our index
-				}
-				else {
-					tokenisedLine += std::to_string(dictionary[word]) + " "; //Adds this words token to the line
-				}
-			}
-
-
-			filterString = filterString.substr(markBPos + 1);
-			markAPos = filterString.find('"');
-
 		}
-
-		tokenisedLine += eolContent;
-
-		tokenisedLines.emplace_back(tokenisedLine);
+		closedir(directory);
 	}
 
-	std::map<int, std::string> outputDict;
-
-	for (auto& el : dictionary) {
-		outputDict[el.second] = el.first;
-	}
 
 	std::cout << "--------------------------------------------------" << std::endl;
 	for (auto& word : outputDict) {
 		std::cout << word.first << ":\t" << word.second << std::endl;
 	}
 
-
-	std::ofstream output("TokenedDialog.txt");
-	
-	for (auto& tl : tokenisedLines) {
-		std::cout << tl << std::endl;
-		output << tl << "\n";
-	}
 
 	std::ofstream outDictFile("Dictionary.csv");
 
@@ -201,7 +206,7 @@ int main() {
 
 		dictionaryContents.emplace_back(nl);
 	}
-	
+
 	for (int i = 0; i < dictionaryContents[0].size(); i++) {
 		outDictFile << dictionaryContents[0][i] << ",";
 	}
@@ -219,12 +224,4 @@ int main() {
 	}
 
 	outDictFile.close();
-
-	//for (auto& e : outputDict)
-	//{
-	//	outDictFile << e.first << ", " << e.second << "\n";
-	//}
-
-
-	
 }
